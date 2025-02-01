61de84d12f2b77b67fb05d56dc0e82673a43e0fe
SoofiBD <burakduvakli@hotmail.com>
2025-02-01 15:17:14 +0300

    Update app.py and templates for improved UI and user experience with Tailwind CSS

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import re
 import ollama
 import os
 
-dotenv_path = "C:\\Users\\burak\\OneDrive\\Desktop\\videoSum\\myenv\\.env"
+dotenv_path = "Write your .env file path"
 load_dotenv(dotenv_path=dotenv_path)
 
 API_KEY = os.getenv("API_KEY")
